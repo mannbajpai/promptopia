b33f508c26be467965178b786fcbebd6d63f2cc7
mannbajpai <mannbajpai.grs@gmail.com>
2024-09-09 17:54:14 +0530

    nav component created

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import Feed from '@components/Feed'
 
 const Home = () => {
   return (
-    <div className='text-3xl'>Home</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Discover and Share
+        <br className='max-sm:hidden' />
+        <span className='orange_gradient text-center'>AI Powered Prompts</span>
+      </h1>
+      <p className='desc text-center'>
+        Promptopia is an opensource AI Prompting tool for modern world to discover, create, and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
   )
 }
 
