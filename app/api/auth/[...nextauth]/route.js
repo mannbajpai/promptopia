54c715fd97f37f05231d0c59db69d535c3b8ae4e
mannbajpai <mannbajpai.grs@gmail.com>
2024-09-11 18:11:08 +0530

    database connection made

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,47 +1,49 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { connectToDatabase } from "@utils/db"
-import User from "@models/user"
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
+import { connectToDatabase } from '@utils/db';
+
 const handler = NextAuth({
-    providers:[
+    secret: process.env.NEXTAUTH_SECRET,
+    url: process.env.NEXTAUTH_URL,
+    providers: [
         GoogleProvider({
-            clientId:process.env.OAUTH_CLIENT_ID,
-            clientSecret:process.env.OAUTH_CLIENT_SECRET,
+            clientId: process.env.OAUTH_CLIENT_ID,
+            clientSecret: process.env.OAUTH_CLIENT_SECRET,
         })
     ],
-    async session({session}){
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        });
+    callbacks: {
+        async session({ session }) {
+            // store the user id from MongoDB to session
+            const sessionUser = await User.findOne({ email: session.user.email });
+            session.user.id = sessionUser._id.toString();
 
-        session.user.id = sessionUser._id.toString();
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDatabase();
 
-        return session;
-    },
-    async signIn({profile}){
-        try {
-            await connectToDatabase()
+                // check if user already exists
+                const userExists = await User.findOne({ email: profile.email });
 
-            // If user already exists
-            const userExists = await User.findOne({
-                email: profile.email
-            });
+                // if not, create a new document and save user in MongoDB
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
 
-            // If not, create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+                return true;
+            } catch (error) {
+                console.log("Error checking if user exists: ", error.message);
+                return false
             }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
+        },
     }
 })
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
